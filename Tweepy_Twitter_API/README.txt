6174aa4d1daedaf5bc0708517bd1fcba35ff0ef5
Xuan Zhou <hinn.chou@gmail.com>
2016-08-14 23:51:29 -0400

    Add files via upload

---
+++ Tweepy_Twitter_API/README.txt
@@ -0,0 +1,11 @@
+This function can extract all tweets data from a certain twitter account.
+
+Because the limit of the tweepy, actually you can all extract around 3200 tweets from that account.
+
+The function returns all these tweets in a well list format.
+
+The function can be used as the first step of the data mining, which is extracting and formating data.
+
+
+
+Reference: http://docs.tweepy.org/en/v3.5.0/auth_tutorial.html
---
+++ Tweepy_Twitter_API/Tweepy_Twitter_API.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 14 15:27:38 2016
+
+@author: hinnc
+"""
+
+import tweepy
+import re
+
+
+def get_text_twitter(twitter_id, cs_key, cs_secret, as_token, as_token_secret):
+    """
+    Get maximal amount of twitters from an account(twitter_id),
+    which can be found after the "id_str" in "view page source".
+    The approximate maximum amount is around 3200, 
+    which is limited by tweepy.
+    """
+    auth = tweepy.OAuthHandler(cs_key, cs_secret)
+    
+    auth.set_access_token(as_token, as_token_secret)
+    
+    api = tweepy.API(auth)
+    
+
+    try:
+        user_tweets = tweepy.Cursor(api.user_timeline, id = twitter_id).pages()
+        
+        # list comprehension, which is equal to:
+        #    for tweet in user_tweets:
+        #        twitter_contents.append(tweet)
+        twitter_contents = [tweet for tweet in user_tweets]
+            
+    except tweepy.TweepError as e:
+        return ('Error: ', e)
+            
+    
+    tweet_all_list = []
+    
+    #Compiled the regex and made the regex case-insensitive
+    pattern = "(?<=text\=\").*?(?=\")|(?<=text\=').*?(?=')"
+    regex = re.compile(pattern, flags=re.IGNORECASE)
+    
+    for text_str in twitter_contents:
+        text_str = str(text_str)
+        tweets_text = regex.findall(text_str)
+        tweet_all_list.append(tweets_text)
+    
+    # Retrieved a flat list out of list of lists (Looks like it's also a fast way)  
+    # Template: [item for sublist in l for item in sublist], l is the list to be flattened.
+    tweets_all = [item for sublist in tweet_all_list for item in sublist]
+    
+    return tweets_all
+    
